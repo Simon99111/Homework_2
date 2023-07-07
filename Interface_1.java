9ec1e8921d912b8ecc94e3f77141acdd45655947
Семён Курочкин <sema.kurochkin@mail.ru>
2023-07-08 01:54:34 +0300

    Город, default-метод

--- Interface_1.java
+++ Interface_1.java
@@ -1,7 +1,7 @@
 public interface Interface_1 extends Interface_2{
     int year = 5;
     public void City(); //Город 
-
+    
     default String sleep(){     //default-метод
         return ("Я сплю!");
     } 
